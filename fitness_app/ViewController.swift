6269b048c9fbd44e8db5dbcfdb82e9aae6eae119
Denis Naughton <mjolinor75@hotmail.com>
2020-08-28 13:11:04 -0400

    first commit

---
+++ fitness_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitness_app
+//
+//  Created by Admin on 8/28/20.
+//  Copyright © 2020 DASHtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
